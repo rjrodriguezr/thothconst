1971671771d84a47526cf04b3e0ac5b8be762fc0
rjrodriguezr <rjrodriguezr@gmail.com>
2025-09-12 17:40:25 -0500

    logs

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const constants = require('./constants');
 
+try {
+    const packageJson = require('./package.json');
+    console.log(`--- EXECUTING VERSION ${packageJson.version} VERSION OF thothconst ---`);
+} catch (error) {
+    console.log('Error leyendo package.json:', error.message);
+}
+
 module.exports = constants;
